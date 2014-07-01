6de66a0466fcf8e24a8d80b660ba8f8a120a305a
Jakub Kułak <jakub.kulak@bauermedia.com>
2014-07-01 21:36:52 +0200

    removed unnecessary installs from bash bootstrap

--- server/Bootstrap.sh
+++ server/Bootstrap.sh
@@ -6,17 +6,16 @@ apt-get update
 # ln -fs /Vagrant/www /var/www
 
 # install git 1.9.1
-apt-get install -y git=1:1.9.1-1
+# apt-get install -y git=1:1.9.1-1
 
 # install memcached 1.4.14
-apt-get install -y memcached=1.4.14-0ubuntu9
+# apt-get install -y memcached=1.4.14-0ubuntu9
 
 # install nginx 1.4.6
-apt-get install -y nginx=1.4.6-1ubuntu3
+# apt-get install -y nginx=1.4.6-1ubuntu3
 
 # install node v0.10.25
-apt-get install -y nodejs=0.10.25~dfsg2-2ubuntu1
+# apt-get install -y nodejs=0.10.25~dfsg2-2ubuntu1
 
 # install php5 with apache lib
-apt-get install -y php5=5.5.9+dfsg-1ubuntu4
-
+# apt-get install -y php5=5.5.9+dfsg-1ubuntu4
