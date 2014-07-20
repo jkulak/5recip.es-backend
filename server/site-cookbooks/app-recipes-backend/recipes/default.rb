ef71df0b4e31de295c2b8e392b26ce16f9b3c8cb
Jakub Kułak <jakub.kulak@bauermedia.com>
2014-07-20 02:24:00 +0200

    Move cookbook management to Berkshelf

---
+++ server/site-cookbooks/app-recipes-backend/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: app-recipes-backend
+# Recipe:: default
+#
+# Copyright 2014, WebAsCrazy.net
+#
+# All rights reserved - Do Not Redistribute
+
+include_recipe 'app-recipes-backend::_common_system'
+include_recipe 'app-recipes-backend::database_server'
+include_recipe 'app-recipes-backend::app_server'
+# include_recipe 'app-recipes-backend::static_files_server'
+include_recipe 'app-recipes-backend::drupal'
+# include_recipe 'app-recipes-backend::nodejs'
